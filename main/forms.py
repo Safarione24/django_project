e1154a57b8f2c69f2ec171177e9e37b593a89c1f
stud <stud@it224-2.almetpt.local>
2026-09-08 11:39:16 +0300

    Initial commit

--- main/forms.py
+++ main/forms.py
@@ -1,12 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm
+from main.models import CustomUser
 from django import forms
-from .models import Category, Product
+from .models import *
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['name', 'surname', 'patronimic', 'username', 'email', 'password', 'password1']
 
 class CategoryForm(forms.ModelForm):
     class Meta:
-        model = Category
-        fields = ['name', 'description']  # Добавлено description
+        model = Course
+        fields = ['name', 'description'] 
 
-class ProductForm(forms.ModelForm):
+class OrderForm(forms.ModelForm):
     class Meta:
-        model = Product
-        fields = ['name', 'price', 'description', 'category']
+        model = Order
+        fields = ['course', 'date']
--- main/models.py
+++ main/models.py
@@ -1,17 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True, default='') 
 
+class CustomUser(AbstractUser):
+    name = models.CharField(verbose_name="Имя", max_length=50)
+    surname = models.CharField(verbose_name="Фамилия", max_length=50)
+    patronimic = models.CharField(verbose_name="Отчество", max_length=50)
+
+
+class Course(models.Model):
+    name = models.CharField(verbose_name="Название", max_length=50)
+    description = models.CharField(verbose_name='Описание')
     def __str__(self):
         return self.name
 
-class Product(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.IntegerField()
-    description = models.TextField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+class Order(models.Model):
+    class Status(models.TextChoices):
+        PEDING = 'pending', 'В ожидании'
+        PROCCESSING = 'proccessing', 'В обработке'
+        COMPLETED = 'completed', 'Выполнен'
+    
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
